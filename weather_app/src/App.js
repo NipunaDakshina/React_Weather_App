f554d4ec3db391b176efb96d20606385e9eae33e
NipunaDakshina <87487225+NipunaDakshina@users.noreply.github.com>
2023-12-20 15:53:40 +0530

    Add login signup UI form

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import LoginSignup from './Components/LoginSignUp/LoginSignup';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <LoginSignup/>
     </div>
   );
 }
